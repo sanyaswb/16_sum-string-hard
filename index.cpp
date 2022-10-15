ab2fe6cdd4ee60253a1556922913653004a30f69
sanyaswb <sanyaswb@gmail.com>
2022-10-15 20:46:41 +0300

    comleted sum string hard

--- index.cpp
+++ index.cpp
@@ -3,5 +3,29 @@
 #include <cmath>
 
 int sumString(const std::string& str) {
-	return 0;
+	int sum = 0;
+	int negative = false;
+
+	for (int i = 0; i < str.length(); ++i) {
+		int code;
+
+		if (str[i] >= 48 && str[i] <= 57) {
+			code = str[i] - '0';
+		} else if (str[i] == 45) {
+			negative = true;
+			continue;
+		} else {
+			negative = false;
+			continue;
+		}
+
+		if (negative) {
+			code *= -1;
+			negative = false;
+		}
+
+		sum += code;
+	}
+
+	return sum;
 }
